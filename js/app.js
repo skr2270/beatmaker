5115c2099605428f8838bf8020f4568f7cd17f21
Sai Kumar Rayavarapu <sk.r.2270@gmail.com>
2021-03-20 13:20:53 +0530

    Added provision to listen composed beats

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ class DrumKit {
         this.snareAudio = document.querySelector('.snare-sound');
         this.hihatAudio = document.querySelector('.hihat-sound');
         this.index = 0;
-        this.bpm = 150;
+        this.bpm = 200;
     }
     activePad() {
         this.classList.toggle("active");
@@ -14,6 +14,25 @@ class DrumKit {
     repeat() {
         let step = this.index % 8;
         const activeBars = document.querySelectorAll(`.b${step}`);
+        //Loop over the pads
+        activeBars.forEach(bar => {
+            bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+            // Check if pads are active
+            if (bar.classList.contains('active')) {
+                if (bar.classList.contains('kick-pad')) {
+                    this.kickAudio.currentTime = 0;
+                    this.kickAudio.play();
+                }
+                if (bar.classList.contains('snare-pad')) {
+                    this.snareAudio.currentTime = 0;
+                    this.snareAudio.play();
+                }
+                if (bar.classList.contains('hihat-pad')) {
+                    this.hihatAudio.currentTime = 0;
+                    this.hihatAudio.play();
+                }
+            }
+        });
         // console.log(activeBars, this.index);
         this.index++;
     }
@@ -29,6 +48,9 @@ const drumKit = new DrumKit();
 
 drumKit.pads.forEach(pad => {
     pad.addEventListener("click", drumKit.activePad);
+    pad.addEventListener("animationend", function () {
+        this.style.animation = "";
+    });
 });
 // drumKit.playBtn.addEventListener("click", () => {
 //     drumKit.start();
